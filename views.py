530d05b86f7ed2c1488a5c3cf85b79e27354daa4
ChuckySSJ <ChuckySSJ.pepito_chivaz10@hotmail.com>
2021-04-06 09:06:12 -0500

    segunda version

--- views.py
+++ views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template,Context
 
 # Create your views here.
+def index(request):
+	contenido = """
+		<html>
+			<head>
+				<title>lopez renteria jose manuel</title>
+			<head>
+			<body>
+				<h1>lopez renteria jose manuel</h1>
+			</body>
+			<style>	
+				body{
+					background-color: red;
+					color: white;
+					font-size: 30px; 
+					text-align:center;
+				}
+
+			</style>
+		</html>		
+	"""
+	return HttpResponse(contenido)
